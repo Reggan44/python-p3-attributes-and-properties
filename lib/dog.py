8d3dda2d0bf7615776bc170c724774e2ddc6ce62
Reggan Nzuki <wambuareggan@gmail.com>
2025-08-09 13:25:30 +0300

    dog2

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,20 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    approved_breeds = [
+        "Labrador Retriever", "German Shepherd", "Golden Retriever", "French Bulldog", "Bulldog",
+        "Poodle", "Beagle", "Rottweiler", "Yorkshire Terrier", "Boxer"
+    ]
+
+    def __init__(self, name=None, breed=None, **kwargs):
+        if name is not None:
+            if (
+                not isinstance(name, str)
+                or name == ""
+                or (isinstance(name, str) and len(name) > 25)
+            ):
+                print("Name must be string between 1 and 25 characters.")
+        self.name = name
+        if breed is not None and breed not in Dog.approved_breeds:
+            print("Breed must be in list of approved breeds.")
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        if name is not None:
+            if (
+                not isinstance(name, str)
+                or name == ""
+                or (isinstance(name, str) and len(name) > 25)
+            ):
+                print("Name must be string between 1 and 25 characters.")
+                self.name = name
+            else:
+                self.name = name.title()
+        else:
+            self.name = name
+        if job is not None and job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        self.job = job
